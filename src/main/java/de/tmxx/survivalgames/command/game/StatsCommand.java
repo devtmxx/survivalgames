68b207c70b68aeea37500126f93be24ec35d6064
Tim Mauersberger <tim-mauersberger@gmx.de>
2025-02-27 18:43:05 +0100

    Added a command to display stats

---
+++ src/main/java/de/tmxx/survivalgames/command/game/StatsCommand.java
@@ -0,0 +1,84 @@
+package de.tmxx.survivalgames.command.game;
+
+import com.google.inject.Inject;
+import de.tmxx.survivalgames.command.Command;
+import de.tmxx.survivalgames.stats.Stats;
+import de.tmxx.survivalgames.stats.StatsKey;
+import de.tmxx.survivalgames.stats.StatsService;
+import de.tmxx.survivalgames.user.User;
+import de.tmxx.survivalgames.user.UserRegistry;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+
+import java.text.NumberFormat;
+
+import static de.tmxx.survivalgames.command.util.CommandSnippets.*;
+
+/**
+ * Project: survivalgames
+ * 27.02.25
+ *
+ * @author timmauersberger
+ * @version 1.0
+ */
+@de.tmxx.survivalgames.command.Game
+public class StatsCommand implements Command {
+    private final UserRegistry registry;
+    private final StatsService statsService;
+
+    @Inject
+    StatsCommand(UserRegistry registry, StatsService statsService) {
+        this.registry = registry;
+        this.statsService = statsService;
+    }
+
+    @Override
+    public String name() {
+        return "stats";
+    }
+
+    @Override
+    public void execute(CommandSourceStack source, String[] args) {
+        User user = getUser(source, registry);
+        if (user == null) return;
+
+        if (args.length == 0) {
+            user.retrieveStats(stats -> showStats(user, user.getName(), stats));
+        } else {
+            String name = args[0];
+            Stats stats = statsService.loadStats(name);
+            if (stats == null) {
+                user.sendMessage("command.stats.not-found", name);
+                return;
+            }
+
+            showStats(user, name, stats);
+        }
+    }
+
+    private void showStats(User user, String playerName, Stats stats) {
+        int kills = stats.get(StatsKey.KILLS);
+        int deaths = stats.get(StatsKey.DEATHS);
+        int wins = stats.get(StatsKey.WINS);
+        int gamesPlayed = stats.get(StatsKey.GAMES_PLAYED);
+        float killDeathRation = calculateRatio(kills, deaths);
+        float winChance = calculateRatio(wins, gamesPlayed);
+
+        NumberFormat format = NumberFormat.getInstance();
+        format.setMaximumFractionDigits(2);
+
+        user.sendMessage(
+                "command.stats",
+                playerName,
+                kills,
+                deaths,
+                format.format(killDeathRation),
+                wins,
+                gamesPlayed,
+                format.format(winChance)
+        );
+    }
+
+    private float calculateRatio(int numerator, int denominator) {
+        return denominator == 0 ? numerator : (float) numerator / denominator;
+    }
+}
