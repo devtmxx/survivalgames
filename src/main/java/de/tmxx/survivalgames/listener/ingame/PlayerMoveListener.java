6599a3d686e49923b565026645930d3478922573
Tim Mauersberger <tim-mauersberger@gmx.de>
2025-03-05 11:57:00 +0100

    Improved pre-game position lock

--- src/main/java/de/tmxx/survivalgames/listener/ingame/PlayerMoveListener.java
+++ src/main/java/de/tmxx/survivalgames/listener/ingame/PlayerMoveListener.java
@@ -1,6 +1,7 @@
 package de.tmxx.survivalgames.listener.ingame;
 
 import de.tmxx.survivalgames.module.game.phase.Starting;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -18,6 +19,10 @@ public class PlayerMoveListener implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         // teleport the player to where he came from if he changed block positions while the game is running the
         // starting countdown
-        if (event.hasChangedBlock()) event.getPlayer().teleport(event.getFrom());
+        Location from = event.getFrom();
+        Location to = event.getTo();
+        if (from.getBlockX() != to.getBlockX() || from.getBlockY() != to.getBlockY()) {
+            event.getPlayer().teleport(event.getFrom());
+        }
     }
 }
